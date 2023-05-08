a4c26cd9263091367c519e4cb83adffc1fd998db
Ebri Goodness <121ebrinix@gmail.com>
2023-05-08 15:42:24 -0700

    Update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,13 +8,10 @@
  *
  * Return: 0 on success, exit with code 97-100 on failure
  */
- 
-#define BUFFER_SIZE 1024
-
 int main(int argc, char *argv[])
 {
-	int file_desc_from, file_desc_to, _read, _write;
-	char buffer[BUFFER_SIZE];
+	int from, to, r, w;
+	char *buffer;
 
 	if (argc != 3)
 	{
@@ -22,52 +19,33 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	file_desc_from = open(argv[1], O_RDONLY);
-	if (file_desc_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	file_desc_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (file_desc_to == -1)
+	while (r > 0)
 	{
-		close(file_desc_from);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-
-	while ((_read = read(file_desc_from, buffer, BUFFER_SIZE)))
-	{
-		if (_read == -1)
+		if (from == -1 || r == -1)
 		{
-			close(file_desc_from);
-			close(file_desc_to);
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
 			exit(98);
 		}
-
-		_write = write(file_desc_to, buffer, _read);
-		if (_write == -1 || _write != _read)
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
 		{
-			close(file_desc_from);
-			close(file_desc_to);
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
 			exit(99);
 		}
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 	}
-
-	if (close(file_desc_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_desc_from);
-		exit(100);
-	}
-
-	if (close(file_desc_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_desc_to);
-		exit(100);
-	}
-
+	free(buffer);
+	close_file(from);
+	close_file(to);
 	return (0);
 }
